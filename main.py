812a59e7991486f2149a61f80b6664227fe8c2cb
Jude Dinoso <jude.dinoso@gmail.com>
2022-11-20 15:32:27 +0900

    move app to main

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from .src.database import Database
+from .src.models.login import Login
+
+app = FastAPI()
+database = Database()
+
+@app.post("/login/")
+async def login(first_name: str, password: str) -> bool:
+    login_params = Login(first_name=first_name, password=password)
+    return database.login(login_params)
+    pass
+
+
