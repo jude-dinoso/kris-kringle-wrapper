71aee5eaad0f1644ba370588ba5e46b9e269244c
jude-dinoso <101120658+jude-dinoso@users.noreply.github.com>
2022-11-23 01:21:35 +0900

    Update CORS option for FastAPI

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from src.database import Database
 from src.models.login import Login
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "https://sjreunion.vercel.app/kris-kringle",
+    "https://sjreunion.vercel.app/dashboard/default",
+    "https://sjreunion.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 database = Database()
 
 @app.post("/login/")
