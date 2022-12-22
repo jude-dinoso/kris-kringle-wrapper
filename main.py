2072a33c038064e7e1bc49b455ae95e1c3ee5c3e
Jude Dinoso <jude.dinoso@gmail.com>
2022-12-23 02:03:09 +0900

    Add santos version

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ origins = [
     "https://sjreunion.vercel.app/dashboard/default",
     "https://sjreunion.vercel.app",
     "https://kris-kringle-9s1jkog07-jude-dinoso.vercel.app",
+    "https://santosreunion.vercel.app",
+    "https://santosreunion.vercel.app/kris-kringle",
+    "https://santosreunion.vercel.app/dashboard/default"
 ]
 
 app.add_middleware(
@@ -64,4 +67,48 @@ async def update_guess(first_name: str, guess: str) -> str:
     update_query = f"UPDATE users SET guess = '{guess}' WHERE first_name = '{first_name}'"
     if database.execute_sql(update_query):
         return database.get_user_data(first_name)
+    raise HTTPException(status_code=500, detail="Connection Error")
+
+##########################################################################
+
+@app.post("/login_s/")
+async def login(first_name: str, password: str):
+    login_params = Login(first_name=first_name, password=password)
+    if database.login(login_params, table="santos_login"):
+        return database.get_user_data(first_name)
+    raise HTTPException(status_code=404, detail="Invalid User/Password")
+
+
+@app.get("/participants_s/")
+async def get_participants() -> list[str]:
+    return database.get_participants(table="santos_users")
+
+@app.get("/description_all_s/")
+async def get_description() -> list[str]:
+    return database.get_name_and_description(table="santos_users")
+
+
+@app.post("/wishlist_s/")
+async def update_wishlist(
+    first_name: str, wish_list: str, wish_list_2: str, wish_list_3: str
+) -> str:
+    update_query = f"UPDATE santos_users SET wish_list = '{wish_list}', wish_list_2 = '{wish_list_2}', wish_list_3 = '{wish_list_3}' WHERE first_name = '{first_name}'"
+    if database.execute_sql(update_query):
+        return database.get_user_data(first_name, table="santos_users")
+    raise HTTPException(status_code=500, detail="Connection Error")
+
+
+@app.post("/description_s/")
+async def update_description(first_name: str, description: str) -> str:
+    update_query = f"UPDATE santos_users SET description = '{description}' WHERE first_name = '{first_name}'"
+    if database.execute_sql(update_query):
+        return database.get_user_data(first_name, table="santos_users")
+    raise HTTPException(status_code=500, detail="Connection Error")
+
+
+@app.post("/guess_s/")
+async def update_guess(first_name: str, guess: str) -> str:
+    update_query = f"UPDATE santos_users SET guess = '{guess}' WHERE first_name = '{first_name}'"
+    if database.execute_sql(update_query):
+        return database.get_user_data(first_name, table="santos_users")
     raise HTTPException(status_code=500, detail="Connection Error")
--- src/database.py
+++ src/database.py
@@ -18,24 +18,24 @@ class Database:
     def create_session(self) -> None:
         self.session = sessionmaker(bind=self.engine)
 
-    def login(self, login_params: Login) -> bool:
-        login_query = f"SELECT * FROM login WHERE first_name = '{login_params.first_name}' AND password = '{login_params.password}'"
+    def login(self, login_params: Login, table: str = "login") -> bool:
+        login_query = f"SELECT * FROM {table} WHERE first_name = '{login_params.first_name}' AND password = '{login_params.password}'"
         data = self.engine.execute(login_query).all()
         if len(data) == 1:
             return True
         return False
 
-    def get_participants(self):
-        select_query = "SELECT first_name from users;"
+    def get_participants(self, table: str = "users"):
+        select_query = f"SELECT first_name from {table};"
         data = self.engine.execute(select_query)
         if data:
             return data.all()
         raise HTTPException(status_code=500, detail="Connection Error")
 
-    def get_user_data(self, first_name: str):
+    def get_user_data(self, first_name: str, table: str = "users"):
         select_query = "SELECT u1.recipient, u1.wish_list, u1.wish_list_2, u1.wish_list_3," \
                        " u2.wish_list as r_wl, u2.wish_list_2 as r_wl2, u2.wish_list_3 as r_wl3, u1.description, " \
-                       "u1.guess from users u1 JOIN users u2" \
+                       f"u1.guess from {table} u1 JOIN {table} u2" \
                        " on u1.recipient = u2.first_name" \
                        f" WHERE u1.first_name = '{first_name}';"
         data = self.engine.execute(select_query)
@@ -43,8 +43,8 @@ class Database:
             return data.first()
         raise HTTPException(status_code=500, detail="Connection Error")
 
-    def get_name_and_description(self):
-        select_query = "SELECT first_name, description from users order by first_name;"
+    def get_name_and_description(self, table: str = "users"):
+        select_query = f"SELECT first_name, description from {table} order by first_name;"
         data = self.engine.execute(select_query)
         if data:
             return data.all()
@@ -55,3 +55,6 @@ class Database:
         if data:
             return True
         raise HTTPException(status_code=500, detail="Connection Error")
+
+
+
